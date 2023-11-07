bd063e3e4fa94365b70fd30e5b1a5ba83da21a8e
Olivier Jargoyhen <olivier.jargoyhen@sfr.fr>
2023-11-07 14:26:30 +0100

    étape 4 grille photo

--- front-page.php
+++ front-page.php
@@ -27,7 +27,26 @@ get_header();
         </div>
     </div>
     <div id="catalogue-container">
-
+        <div id="catalogue-photo-container">
+        <?php 
+    // Image aléatoire
+        // 1. On définit les arguments pour définir ce que l'on souhaite récupérer
+        $args2 = array(
+            'post_type' => 'photos',
+            'posts_per_page' => 12,
+            'orderby' => 'rand'                       
+        );
+        // 2. On exécute la WP Query
+        $my_query = new WP_Query( $args2 );
+        // 3. Boucle   
+        if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+                get_template_part('template-parts/photo_block');
+            endwhile;
+        endif;
+        // 4. On réinitialise à la requête principale (important)
+        wp_reset_postdata();
+        ?>
+        </div>
     </div>
 </div>
 <?php
